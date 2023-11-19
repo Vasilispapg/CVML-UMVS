caaa328fa4f2f075748ddc1122cb59383bb29fa3
Vasileios Savvas Papagrigoriou <vasilispapg@outlook.com>
2023-11-19 12:24:06 +0200

    ObjectDetection using YOLOv3

---
+++ objectDetection.py
@@ -0,0 +1,68 @@
+import os
+import cv2
+import numpy as np
+
+def detect_objects_from_frames(frames, cluster_labels, model, classes):
+    """
+    Detect unique objects in video frames grouped by cluster labels.
+    
+    Args:
+    frames: List of video frames.
+    cluster_labels: Cluster labels for each frame.
+    model: The loaded YOLO model.
+    classes: A list of class names that YOLO can detect.
+
+    Returns:
+    A dictionary of unique detected objects grouped by cluster label.
+    """
+    print("Object detection starts")
+    
+    cluster_labels = cluster_labels.flatten()  # This converts a 2D array to a 1D array
+   
+    # set of unique cluster labels
+    clustered_frames = {label: [] for label in set(cluster_labels)}
+    
+    # attach each frame in the video to its corresponding cluster label
+    for label, frame in zip(cluster_labels, frames):
+        # Add 50 frames to each cluster label
+        for _ in range(100):
+            # Append the frame to the list of frames for the corresponding cluster label
+            clustered_frames[label].append(frame)
+
+    # detect objects in each frame of the video
+    detected_objects = {label: set() for label in clustered_frames}
+    for label, frames in clustered_frames.items():
+        for frame in frames:
+            detected_labels = detect_objects_with_yolo(frame, model, classes)
+            for obj in detected_labels:
+                detected_objects[label].add(obj)
+
+    return {label: list(objects) for label, objects in detected_objects.items()}
+
+def detect_objects_with_yolo(frame, model, classes):
+    """
+    Detect objects in a frame using the YOLO model.
+
+    Args:
+    frame: A single frame to perform object detection on.
+    model: The loaded YOLO model.
+    classes: A list of class names that YOLO can detect.
+
+    Returns:
+    A list of detected object labels in the frame.
+    """
+    height, width, _ = frame.shape
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), swapRB=True, crop=False) # type: ignore
+    model.setInput(blob)
+    outputs = model.forward(model.getUnconnectedOutLayersNames())
+  
+    detected_labels = set()
+    for output in outputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.95:  # Confidence threshold
+                detected_labels.add(classes[class_id])
+
+    return list(detected_labels)
